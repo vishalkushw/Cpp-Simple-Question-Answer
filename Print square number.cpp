2c0dd52001a441f774ed14051d0858d6ce2e5274
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-18 00:11:31 +0530

    Print square number

---
+++ Print square number.cpp
@@ -0,0 +1,10 @@
+// print the square number 
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter number :";
+	cin>>num;
+	cout<<"Your square number is :";
+	cout<<num*num;
+}
