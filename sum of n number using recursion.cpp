e037c0208dfd36e874323c74ffdd77dc3512fdc4
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-24 21:04:23 +0530

    sum of n number using recursion

---
+++ sum of n number using recursion.cpp
@@ -0,0 +1,20 @@
+// sum of n
+
+#include <iostream>
+using  namespace std;
+
+int Sum(int n){
+	if(n==0){
+		return 0;
+	}
+	
+	int prevSum=Sum(n-1);
+	return n+prevSum;
+}
+int main(){
+	int n;
+	cout<<"enter number : ";
+	cin>>n;
+	cout<<Sum(n);
+}
+
