391d5da66f4b82a4c4e51e63b7520aeedf5cec07
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-20 01:00:40 +0530

    Calculate the electricity bill

---
+++ If else/Calculate the electricity bill .cpp
@@ -0,0 +1,28 @@
+  /*  Write a C++ program to calculate the electricity bill 
+  based on the following rates:
+Units <= 100: Rate per unit = Rs 1
+100 < Units <= 200: Rate per unit = Rs 1.5
+Units > 200: Rate per unit = Rs 2    */ 
+		
+		#include <iostream>
+		using namespace std;
+		int main(){
+			int units;
+			cout<<"Enter your unit : " ;
+			cin>>units;
+			
+			if(units<=100){
+				cout<<"rate per units rs 1";
+				
+			}
+			else if(units>00 && units<=200){
+			
+				cout<<"rate per units rs 1.5 ";
+				
+			}
+			else if(units>200){
+			
+				cout<<"rate per unit rs 2 :";
+			}
+		}
+		
