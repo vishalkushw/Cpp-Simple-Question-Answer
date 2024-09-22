7190aeefa95e8edcae9c7718f530d3aed6cdb30b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-22 11:21:14 +0530

    It is vowel or not

---
+++ It is vowel or not.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+        using namespace std;
+        int main(){
+            char A;
+	        cout <<"Enter you character : ";
+	        cin >>A;
+	        if(A == 'a'||A=='e'||A=='i'||A=='o'||A=='u'||A=='A'||A=='E'||A=='I'||A=='O'||A=='U'){
+		        cout<<"it is vowel : "<<A;
+	            }
+	           else{
+		        cout<<"it is consonant : "<<A;
+               	}
+          }
+          
