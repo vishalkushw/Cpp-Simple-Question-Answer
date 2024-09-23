26518b1c31c632d6d67b4e9609f4dbf90a095b6b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-23 18:37:28 +0530

    String length and add srring

---
+++ String length and add srring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string f1= "PineApple",new_string="";
+	int size= f1.size();
+	cout<<f1.length()<<endl;
+	
+	new_string=new_string+" now";
+	cout<<new_string<<endl;
+	
+	new_string=new_string+" or never";
+	cout<<new_string<<endl;
+		
+}
