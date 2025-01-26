45e63ff99a53a75c7dc2f693f8df120df9eadd2f
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-01-26 23:35:18 +0530

    Account function

---
+++ Account function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void roll(int roll){
+	
+	 cout<<"Your accunt number is : 9234567 ";
+	
+}
+
+int main(){
+	int r;
+	cout<<"Enter rollnumber : ";
+	cin>>r;
+   
+   roll(r);
+	
+}
